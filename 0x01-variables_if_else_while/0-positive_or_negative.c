48c0390c1b058e8326b7d54ea1eee7035667d915
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-11 10:25:29 -0800

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,11 +22,11 @@ int main(void)
 		
 	}
 	else if (n < 0)
-		printf("%d is negative\n", n)
+		printf("%d is negative\n", n);
 	}
 	else
 	{		
 		printf("%d is zero\n", n);
-											}
+	}
 	return (0);
-       }
+  }
