bb762e94e6c47122a5833172ee45db6649a3f076
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-12 14:17:18 -0800

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,8 +10,27 @@
 
 int main(void)
 {
-	int = 0;
-	int 
+	int i = 0;
+	int j;
+	int count = 0;
+
+	while (i <= 8)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (count != 44)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+			count++;
+		}
+		i++;
+	}
 	putchar('\n');
 	return (0);
 }
