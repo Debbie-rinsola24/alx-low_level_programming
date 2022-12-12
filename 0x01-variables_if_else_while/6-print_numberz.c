4bd15be7a1e65171dd9202461f9d95a439987a2a
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-12 07:43:45 -0800

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,7 +4,7 @@
  * main - entry point
  * @void: null value
  *
- * Description: Print numbers from 0 to 9
+ * Description: print numbers from 0 to 9
  * Return: zero value
  */
 
@@ -12,9 +12,9 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
-		printf("%d", n);
-	printf("\n");
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
 	return (0);
 }
 
