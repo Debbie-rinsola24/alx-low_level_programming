4c324b1ede05fdbefbfe253c8937d08c9f7ba44f
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-14 15:09:12 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,16 +12,16 @@ void print_alphabet_x10(void)
 
 	i = 0;
 
-	while (count < 10)
+	while (i < 10)
 	{
 		ch = 'a';
 		while (ch <= 'z')
 		{
-			_putchar(c);
+			_putchar(ch);
 			ch++;
 		}
 		_putchar('\n');
-		i++
+		i++;
 	}
 }
 
