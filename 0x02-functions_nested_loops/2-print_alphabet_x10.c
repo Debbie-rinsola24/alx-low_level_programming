c478008706808e9f51dada63185c544bac13a690
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-13 20:50:41 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,23 +2,26 @@
 
 /**
  * print_alphabet_x10 - print in lowercase x10
+ * followed by a new line
  */
 
 void print_alphabet_x10(void)
 {
-	int c = 'a'
-	int count = 0;
+	char ch;
+	int i;
+
+	i = 0;
 
 	while (count < 10)
 	{
-		while (c <= 'z')
+		ch = 'a';
+		while (ch <= 'z')
 		{
 			_putchar(c);
-			c++;
+			ch++;
 		}
-		c = 'a'
-		count++;
 		_putchar('\n');
+		i++
 	}
 }
 
