e83019d9413023fc4ac64a0097d4e155f9e21088
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-14 17:08:59 -0800

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
-int main(void)
+int _isalpha(int c)
 {
-		int r;
-
-		r =  _islower('H');
-		_putchar(r + '0');
-		r =  _islower('o');
-		 _putchar(r + '0');
-		r = _islower(108);
-		_putchar(r + '0');
-		 r = _isalpha(';');
-		 _putchar(r + '0');
-		_putchar('\n');
-		return (0);
+	return ((c >= 'a' && c <= 'z') || (c <= 'z'));
 }
-
--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,6 +1,4 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
  *
