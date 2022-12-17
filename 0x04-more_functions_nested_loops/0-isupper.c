9c1cb43fd11fe3824b9c1c33d9354f37b52b26b3
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-16 19:22:40 -0800

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupplier - check caracter
+ * @c: Character for checking
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise
+ */
+
+int _isupplier(int c)
+{
+	return (c >= 'A' && C <= 'Z');
+}
