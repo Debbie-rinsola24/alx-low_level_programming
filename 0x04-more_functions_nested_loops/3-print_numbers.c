764d37441d9e6f744f83a91af2c64701fcd1ef44
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-16 20:11:18 -0800

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - print num from 0 - 9
+ * Return: void
+ */
+void print_number(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n')
+}
