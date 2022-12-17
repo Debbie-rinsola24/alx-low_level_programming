71a1e6feb597669c41496a997b5ce5ae47626d06
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-16 20:44:10 -0800

    task2

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -10,7 +10,7 @@ int main(void)
 
 {
 	
-printf("%d\n", mul(98, 1024))
+printf("%d\n", mul(98, 1024));
 printf("%d\n", mul(-402, 4096));
 return (0);
 }
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,13 @@
+#include "main.h"
 
+/**
+ * mul - mutiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplication
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
