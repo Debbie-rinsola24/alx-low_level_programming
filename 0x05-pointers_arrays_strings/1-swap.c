574a5013288a9c324c833f41cad608c9558aedbd
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-19 05:14:38 -0800

    task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int -> given two integers swap the values they are holding 
+ * swap_int -> given two integers swap the values they are holding
  * @a: parameter 1
  * @b: parameter 2
  */
