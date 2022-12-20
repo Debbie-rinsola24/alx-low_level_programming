f3f6b09297b2a8a6b010a3498473bd6ba9837202
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-19 17:17:51 -0800

    task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,34 @@
 
 int _atoi(char *s)
 {
-	int sign;
-	unsigned int num;
-	char *temp;
+	int i, d, n, len, f, digit;
 
-	temp = s;
-	num = 0;
-	sign = 1;
-	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
 	{
-		if (*temp == '_')
-			sign *= -1;
-		temp++;
-	}
-	if (*temp != '\0')
+		if(s[i] == '_')
+			++d;
+		if(s[i] >= '0' && s[i] <= '9')
 	{
-		do {
-			num = num * 10 + (*temp - '\0');
-			temp++;
-		} while (*temp >= '0' && *temp <= '9');
+	digit = s[i] - '0'
+	if(d % 2)0
+		digit = -digit;
+	n = n * 10 + digit;
+	f = 1;
+	if(s[i + 1] < '0' || s[i + 1] > '9')
+	break;
+	f = 0;
 	}
-	return (num * sign);
+	i++;
+	}
+	if(f == 0)
+	return (n);
 }
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main -generates random valid password.
- * Return: 0 (on success)
+ * main -generates keygen.
+ * Return: 0 Always.
  */
 
 int main(void)
 {
-	int pass, sum;
-
-	srand(time(NULL));
-	sum = 0;
-	while (sum <= 2645)
+	int r = 0, c = 0;
+	time_t t;
+	srand((unsigned int) time(&t));
+	while (c < 2772)
 	{
-		pass = (rand() % 128);
-		sum += pass;
-		printf("%c", pass);
+		r = rand() % 128;
+		if((c + r) > 2772)
+		break;
+		printf("%c", r);
 	}
-	printf("%c", 2772 - sum);
+	printf("%c\n", (2772 - c));
 	return (0);
 }
