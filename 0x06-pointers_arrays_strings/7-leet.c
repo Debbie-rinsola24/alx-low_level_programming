357ca8085c38a0cc51d2df54d7fbd9b4a8798788
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-22 09:08:07 -0800

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encode a string into 1337
+ * @s: input
+ *
+ * Return: address of s
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+			*(s + i) = b[j];
+		}
+	}
+	return (s);
+}
