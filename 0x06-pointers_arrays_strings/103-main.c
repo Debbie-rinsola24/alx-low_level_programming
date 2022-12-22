0ec927d58092bcc4f86dfea3a1fb5c73fbacbc1a
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-22 15:27:36 -0800

    task11

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -18,59 +18,43 @@ char *m = "8693246597324659762347956349265983465962349569346";
 
 	res = infinite_add(n, m, r, 100);
 	if (res == 0)
-	{											                printf("Error\n");
-	}
-	else												    {
-		printf("%s + %s = %s\n", n, m, res)
-	}
-	n = "1234567890";									
-	m = "1";											        res = infinite_add(n, m, r2, 10);
-	if (res == 0)
 	{
 		printf("Error\n");
+	}
 	else
 	{
 		printf("%s + %s = %s\n", n, m, res);
 	}
-	n = "999999999";															        m = "1";
-																	        res = infinite_add(n, m, r2, 10);
-
-																		        if (res == 0)
-
-																				        {
-
-																						                printf("Error\n");
-
-																								        }
-
-																			        else
-
-																					        {
-
-																							                printf("%s + %s = %s\n", n, m, res);
-
-																									        }
-
-																				        res = infinite_add(n, m, r3, 11);
-
-																					        if (res == 0)
-
-																							        {
-
-																									                printf("Error\n");
-
-																											        }
-
-																						        else
-
-																								        {
-
-																										                printf("%s + %s = %s\n", n, m, res);
-
-																												        }
-
-																							        return (0);
-
+	n = "1234567890";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	n = "999999";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	res = infinite_add(n, m, r3, 11);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	return (0);
 }
-
-j
