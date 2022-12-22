bd33ffa3417c925c8392636544c9f5fa87719e4a
Debbie-rinsola24 <rinsolatiti@gmail.com>
2022-12-22 06:24:00 -0800

    task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes words
+ * @s: string to change to uppercase
+ * Return: pointer to string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z' &&
+		 	(s[i - 1] == ' ' ||
+			s[i - 1] == '\t' ||
+			s[i - 1] == '\n' ||
+			s[i - 1] == ',' ||
+			s[i - 1] == ';' ||
+			s[i - 1] == '.' ||
+			s[i - 1] == '!' ||
+			s[i - 1] == '?' ||
+			s[i - 1] == '"' ||
+			s[i - 1] == '(' ||
+			s[i - 1] == ')' ||
+			s[i - 1] == '{' ||
+			i == 0))
+			{
+			s[i] = s[i] - 32;
+			}
+			i++;
+		}
+		return (s);
+}
+
