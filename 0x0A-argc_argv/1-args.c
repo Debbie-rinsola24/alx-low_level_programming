524beab70f271987e3043dc481fdf2951c1906a3
Debbie-rinsola24 <rinsolatiti@gmail.com>
2023-01-10 13:51:44 -0800

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print number of arguments passed to program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[] _attribute_((unused)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+	return (0);
+}
