bb16c5db347dd3bd6c3e423099eafb360713bc85
Debbie-rinsola24 <rinsolatiti@gmail.com>
2023-01-10 14:16:59 -0800

    agc

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
- * main - print number of arguments passed to program
+ * main - print each argument passed to program on a line
  * @argc: argument counter
  * @argv: pointer to array of arguments
  * Return: 0 on success
  */
-
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%d\n", argc - 1);
 
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
