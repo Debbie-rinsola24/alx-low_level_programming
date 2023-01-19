13afff7803154b169aeee6834e79329f35fc6354
Debbie-rinsola24 <rinsolatiti@gmail.com>
2023-01-19 15:02:22 -0800

    func1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	register unsigned int i;
 	va_list nl;
-	
+
 	va_start(nl, n);
 	for (i = 0; i < n; i++)
 		printf("%i%s", va_arg(nl, int), (separator && i != n - 1) ? separator : "");
