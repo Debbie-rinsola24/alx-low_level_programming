6922500f2e91306ad425db04b1f2664aebca4fdd
Debbie-rinsola24 <rinsolatiti@gmail.com>
2023-01-27 10:25:28 -0800

    task0

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	list_t *head;
 	list_t *new;
-	list_t hello = {"World", 5, NULl};
+	list_t hello = {"World", 5, NULL};
 	size_t n;
 
 	head = &hello;
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: lenght of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Hlberton project
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
