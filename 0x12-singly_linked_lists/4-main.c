a650b7d2acd7254d54f7fe51011c54da55e08b72
Debbie-rinsola24 <rinsolatiti@gmail.com>
2023-01-27 11:24:48 -0800

    t4

--- 0x12-singly_linked_lists/4-main.c
+++ 0x12-singly_linked_lists/4-main.c
@@ -15,7 +15,7 @@ int main(void)
 	head = NULL;
 	add_node_end(&head, "Bob");
 	add_node_end(&head, "&");
-	add_node_end(&head, "Kris")
+	add_node_end(&head, "Kris");
 	add_node_end(&head, "love");
 	add_node_end(&head, "asm");
 	print_list(head);
