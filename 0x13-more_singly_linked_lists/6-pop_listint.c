3418ad754918f086d147d43347ffef7e63b891d4
Debbie-rinsola24 <rinsolatiti@gmail.com>
2023-01-31 15:48:00 -0800

    t7

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,20 +14,20 @@ int pop_listint(listint_t **head)
 	int hnode;
 	listint_t *h;
 	listint_t *curr;
-	
+
 	if (*head == NULL)
 		return (0);
-	
+
 	curr = *head;
 
 	hnode = curr->n;
 
 	h = curr->next;
-	
+
 	free(curr);
-	
+
 	*head = h;
-	
-	
+
+
 	return (hnode);
 }
